cf5355b06a9c7567fdcafbbbbe2f25fd6dc686e2
davidwolfpaw <david@davidwolfpaw.com>
2024-10-01 18:53:11 -0400

    adds plugin headers to allow you to activate it from the plugin dashboard

---
+++ testimonial-block.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: Testimonial Block Workshop
+ * Description: Basic Custom Testimonial Block with no build tools needed - Workshop Demo
+ * Version: 1.0.0
+ * Author: david wolfpaw
+ */
